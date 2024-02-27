f3ab3c2fe41f3b6b73ce19f1e56bc54b81fa00d1
BalaAtchaya2004 <atchayaramesh574@gmail.com>
2024-02-27 15:05:51 +0530

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyNotes from "./screens/MyNotes/MyNotes";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
+import CreateNote from "./screens/CreateNote/CreateNote";
+import SingleNote from "./screens/SingleNote/SingleNote";
+import { useState } from "react";
+import ProfileScreen from "./screens/ProfileScreen/ProfileScreen";
 
 function App() {
+  const [search, setSearch] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header setSearch={setSearch} />
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginScreen />} />
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/profile" element={<ProfileScreen />} />
+          <Route path="/mynotes" element={<MyNotes search={search} />} />
+          <Route path="/createnote" element={<CreateNote />} />
+          <Route path="/note/:id" element={<SingleNote />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/screens/LandingPage/LandingPage.js
@@ -0,0 +1,49 @@
+import "./landingPage.css";
+import { Button, Container, Row } from "react-bootstrap";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    const userInfo = localStorage.getItem("userInfo")
+
+    if(userInfo){
+      navigate("/mynotes")
+    }
+  }, [navigate])
+
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <div>
+              <h1 className="title">Welcome to <i>Appointment Tracker</i></h1>
+              <p className="subtitle">Keep your records safe</p>
+            </div>
+            <div className="buttonContainer">
+              <Link to="/login">
+                <Button size="lg" className="landingbutton">
+                  Login
+                </Button>
+              </Link>
+              <Link to="/register">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-primary"
+                >
+                  Sign up
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+export default LandingPage;
